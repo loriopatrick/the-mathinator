c6c6b39d3a519963276bbc86a0d8a6f1d472c937
Patrick Lorio <patricklorio.com@gmail.com>
2012-07-06 20:10:15 +0200

    Create ant build for engine Created deploy scripts

--- Deploy/build.py
+++ Deploy/build.py
@@ -1,2 +1,27 @@
 import os
+import shutil
+import settings
 
+LOC = 'out/'
+
+def ex (cmd):
+	return os.popen (cmd).read()
+
+def clean ():
+	print 'cleaning...'
+	ex('rm -r %s' % LOC)
+	ex('mkdir %s' % LOC)
+	print ' ----- done'
+
+def buildEngine ():
+	print 'building...'
+	ex('echo $(cd ../Engine; ant clean; ant build; ant jar)')
+	print ' ------ done'
+
+	print 'copying...'
+	ex('cp ../Engine/build/jar/engine.jar %s' % LOC)
+	print ' ------ done'
+
+
+clean()
+buildEngine()
