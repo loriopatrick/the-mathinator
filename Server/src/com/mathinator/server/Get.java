162380b07f4c1b1b2df2221401f30eeff2053678
Patrick Lorio <patricklorio.com@gmail.com>
2012-08-27 17:40:53 -0700

    added ad to webpages

--- Server/src/com/mathinator/server/Get.java
+++ Server/src/com/mathinator/server/Get.java
@@ -7,15 +7,21 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.logging.StreamHandler;
 
 public class Get implements HttpHandler {
+
+    public Get () {
+        lastMod = new Date().toGMTString();
+    }
 
     public Get(String root) {
         this.root = root;
     }
 
     public String root;
+    public String lastMod;
 
     public void handle(HttpExchange ex) {
         String path = ex.getRequestURI().toString();
@@ -29,14 +35,19 @@ public class Get implements HttpHandler {
         String end = temp[temp.length - 1];
 
         if (end.equals("js")) {
-            h.add("Content-Type", "text/javascript");
-            h.add("charset", "utf-8");
+            h.add("Content-Type", "text/javascript; charset=utf-8");
         } else if (end.equals("html")) {
-            h.add("Content-Type", "text/html");
-            h.add("charset", "utf-8");
+            h.add("Content-Type", "text/html; charset=utf-8");
+        } else if (end.endsWith("css")) {
+            h.add("Content-Type", "text/css; charset=utf-8");
         } else {
-            h.add("charset", "utf-8");
+            h.add("Charset", "utf-8");
         }
+
+        h.add("Server", "Mathinator");
+//        h.add("Date", new Date().toGMTString());
+//        h.add("Accept-Range", "bytes");
+//        h.add("Last-Modified", lastMod);
 
         try {
             InputStream in = getClass().getClassLoader().getResourceAsStream(root + path);
